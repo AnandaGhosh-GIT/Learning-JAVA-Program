15d914269cfd39e60d40491489647c6038518646
Ananda Ghosh <ghoshananda4@gmail.com>
2026-07-08 21:54:33 +0530

    Literals

---
+++ src/ex_03_Literals/Lab009_Interview_Question.java
@@ -0,0 +1,9 @@
+package ex_03_Literals;
+
+public class Lab009_Interview_Question {
+    public static void main(String[] args) {
+        long l = 100l;
+        // How Bytes, Bits ?
+        //  8 Bytes, 64 Bits
+    }
+}
