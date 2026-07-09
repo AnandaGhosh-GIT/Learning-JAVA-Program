27e183240b47019684b04127e7b8556ff037e5db
Ananda Ghosh <ghoshananda4@gmail.com>
2026-07-10 02:30:34 +0530

    Student Pass/Fail using Ternary Operator

---
+++ src/practice_programs/Student_Pass_or_Fail_Using_TernaryOperator.java
@@ -0,0 +1,9 @@
+package practice_programs;
+
+public class Student_Pass_or_Fail_Using_TernaryOperator {
+    public static void main(String[] args) {
+        int marks =20;
+        String result= (marks>=40) ?"PASS":"FAIL";
+        System.out.println(result);
+    }
+}
