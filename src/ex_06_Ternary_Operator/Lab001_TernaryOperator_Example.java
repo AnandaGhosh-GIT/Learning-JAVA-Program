d5a8d1592c54c8cda536501739693af986f576cc
Ananda Ghosh <ghoshananda4@gmail.com>
2026-07-10 21:19:40 +0530

    Ternary Operators

--- src/ex_06_Ternary_Operator/Lab001_TernaryOperator_Example.java
+++ src/ex_06_Ternary_Operator/Lab001_TernaryOperator_Example.java
@@ -2,7 +2,7 @@ package ex_06_Ternary_Operator;
 
 public class Lab001_TernaryOperator_Example {
     public static void main(String[] args) {
-        int age = 7;
+        int age = 20;
         String canBabiGotoGOA = age > 18 ? "Yes" : "No";
         System.out.println("Can Babi go to GOA?: "+canBabiGotoGOA);
 
--- src/ex_06_Ternary_Operator/Lab004_Nested_TernaryOperator.java
+++ src/ex_06_Ternary_Operator/Lab004_Nested_TernaryOperator.java
@@ -2,9 +2,9 @@ package ex_06_Ternary_Operator;
 
 public class Lab004_Nested_TernaryOperator {
     public static void main(String[] args) {
-        int age = 25;
+        int age = 7;
         String canBabiGotoGOA = age > 18 ? (age >= 25 ? "Babi can go Alone": "Babi have to go with parents") : "No Babi can't go to GOA";
-        System.out.println(canBabiGotoGOA);
+        System.out.println("Can Babi goto GOA?: "+ canBabiGotoGOA);
 
     }
 }
