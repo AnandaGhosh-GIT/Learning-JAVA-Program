b05b12294630a7e855866de6ca369d929d96b639
Ananda Ghosh <ghoshananda4@gmail.com>
2026-07-15 12:33:33 +0530

    User Input

--- src/ex_07_User_Input/Lab003_User_Input_through_Scanner.java
+++ src/ex_07_User_Input/Lab003_User_Input_through_Scanner.java
@@ -2,18 +2,13 @@ package ex_07_User_Input;
 
 import java.util.Scanner;
 
-public class Lab003_Voter_classification_through_Scanner {
+public class Lab003_User_Input_through_Scanner {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = input.nextLine();
         System.out.println("Enter your age: ");
         int age = input.nextInt();
-        if (age > 18) {
-            System.out.println(name + " is " + age + ", he/she can vote");
-        }
-        else {
-            System.out.println(name + " is " + age + ", he/she can't vote");
-        }
+        System.out.println(name + age);
     }
 }
