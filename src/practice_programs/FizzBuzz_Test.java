6ab34e036ea6003cb8c9e3ad406ad61e6f8707c6
Ananda Ghosh <ghoshananda4@gmail.com>
2026-07-19 14:59:56 +0530

    Fizz Buzz Test

---
+++ src/practice_programs/FizzBuzz_Test.java
@@ -0,0 +1,13 @@
+package practice_programs;
+
+public class FizzBuzz_Test {
+    ////FizzBuzz Test:
+    //Write a program that prints numbers from 1 to 100.
+    // However, for multiples of 3, print "Fizz" instead of the number,
+    // and for multiples of 5, print "Buzz."
+    // For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+    public static void main(String[] args) {
+        
+    }
+
+}
