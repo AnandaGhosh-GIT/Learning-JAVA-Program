f39c4dc46c1cb2ca78175954cb83c3ef8c82a707
Ananda Ghosh <ghoshananda4@gmail.com>
2026-08-27 22:38:18 +0530

    Fizz Buzz Test

--- src/practice_programs/FizzBuzz_Test.java
+++ src/practice_programs/FizzBuzz_Test.java
@@ -1,13 +1,27 @@
 package practice_programs;
 
+import java.util.Scanner;
+
 public class FizzBuzz_Test {
     ////FizzBuzz Test:
-    //Write a program that prints numbers from 1 to 100.
+    //Write a program that prints numbers from 1 to n(user input).
     // However, for multiples of 3, print "Fizz" instead of the number,
     // and for multiples of 5, print "Buzz."
     // For numbers that are multiples of both 3 and 5, print "FizzBuzz."
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int n= input.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            } else if (i%3==0) {
+                System.out.println("Fizz");
+            } else if (i%5 ==0){
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
+        }
     }
 
 }
