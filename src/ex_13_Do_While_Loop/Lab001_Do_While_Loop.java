cd28360607ea268b03fa8b20f8b58ea67681d161
Ananda Ghosh <ghoshananda4@gmail.com>
2026-09-16 22:57:27 +0530

    Do While Loop

---
+++ src/ex_13_Do_While_Loop/Lab001_Do_While_Loop.java
@@ -0,0 +1,11 @@
+package ex_13_Do_While_Loop;
+
+public class Lab001_Do_While_Loop {
+    public static void main(String[] args){
+        int a=1;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<10);
+    }
+}
---
+++ src/ex_13_Do_While_Loop/Lab002_Difference_between_While_DoWhile.java
@@ -0,0 +1,19 @@
+package ex_13_Do_While_Loop;
+
+public class Lab002_Difference_between_While_DoWhile {
+    public static void main(String args[]){
+        int a=0;
+
+        ////While Loop
+        //while(a<0){
+        //    System.out.println(a);
+        //    a++;
+        //}
+
+        ////Do While Loop
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<0);
+    }
+}
